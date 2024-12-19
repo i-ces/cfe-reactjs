2fc9e71f2ab6f4195c73d63417122280f66081ba
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-19 20:51:49 +0545

    define and array

---
+++ seamoon/day2/array/index.js
@@ -0,0 +1,20 @@
+// defining an array
+
+const arr1 = [1, 2, 3, 4, 5];
+// arr1 => [1, 2, 3, 4, 5]
+
+const arr2 = ["a", "b", "c", "d", "e"];
+// arr2 => ["a", "b", "c", "d", "e"]
+
+const arr3 = ["hello", "world"];
+// arr3 => ["hello", "world"]
+
+const str = "hello world";
+
+const superArr = [arr1, arr2, arr3, str];
+
+console.log("combined array start");
+
+console.log(superArr);
+
+console.log("combined array end");
---
+++ seamoon/day2/array/readme.md
@@ -0,0 +1,8 @@
+# Array
+
+An array is a set of similar data types stored in a single variable. It is a data structure that stores a collection of elements of the same data type. It is a container that holds multiple values in a single variable.
+
+- index starts from zero
+- can be accessed by the array_name[index]
+- array can be anything like int, float, char, etc.
+- array also can be array of arrays
