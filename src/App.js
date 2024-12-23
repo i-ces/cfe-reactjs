d392230c8545136d8d365a902bc8938cf8c3bba1
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-23 21:56:14 +0545

    finalize UI for todo app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
 import Todo from "./components/Todo";
 
+const todos = [
+  {
+    title: "Learn React",
+    description: "React is a JavaScript library for building user interfaces",
+  },
+  {
+    title: "Learn JavaScript",
+    description: "JavaScript is a versatile programming language",
+  },
+  {
+    title: "Learn HTML",
+    description: "HTML is the standard markup language for creating web pages",
+  },
+  {
+    title: "Learn CSS",
+    description:
+      "CSS is a stylesheet language used to describe the presentation of a document",
+  },
+  {
+    title: "Learn Node.js",
+    description:
+      "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine",
+  },
+  {
+    title: "Learn Express.js",
+    description: "Express.js is a web application framework for Node.js",
+  },
+  {
+    title: "Learn MongoDB",
+    description: "MongoDB is a document-oriented NoSQL database",
+  },
+  {
+    title: "Learn GraphQL",
+    description: "GraphQL is a query language for your API",
+  },
+  {
+    title: "Learn TypeScript",
+    description: "TypeScript is a typed superset of JavaScript",
+  },
+  {
+    title: "Learn Redux",
+    description: "Redux is a predictable state container for JavaScript apps",
+  },
+  {
+    title: "Learn Next.js",
+    description: "Next.js is a React framework for production",
+  },
+];
+
 function App() {
   return (
     <>
@@ -24,48 +73,12 @@ function App() {
           alignItems: "center",
         }}
       >
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
-        <Todo
-          description={
-            "React is a JavaScript library for building user interfaces"
-          }
-          title={"Learn React"}
-        />
+        {todos.map((todo, index) => {
+          //   const title = todo.title;
+          //   const description = todo.description;
+          const { title, description } = todo;
+          return <Todo key={index} title={title} description={description} />;
+        })}
       </main>
     </>
   );
