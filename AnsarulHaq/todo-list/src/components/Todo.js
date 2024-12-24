ba1f7f1d149442a8aba37b9b7b9bf0700d29942c
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-12-24 21:55:03 +0545

    added delete button

--- AnsarulHaq/todo-list/src/components/Todo.js
+++ AnsarulHaq/todo-list/src/components/Todo.js
@@ -1,10 +1,21 @@
 import React from "react";
-
-function Todo({title, description}) {
+import "./Todo.css";
+function Todo({ title, description }) {
+  const handleDelete=()=>{
+    alert("delete icon clicked!");
+  };
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="todo">
+      <h2 className="todo--title">
+        {title}
+      </h2>
+      <p className="todo--description">
+        {description}
+      </p>
+      <button className="todo--button-delete" onClick={handleDelete}>
+        <span className="material-symbols-outlined">
+delete
+</span></button>
     </div>
   );
 }
