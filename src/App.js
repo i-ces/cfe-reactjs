bc6ce6dd40deb67678e25fe6e633ecfe834a66d6
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-24 21:15:22 +0545

    migrate css

--- src/App.js
+++ src/App.js
@@ -5,30 +5,12 @@ import { todos } from "./data/todo";
 function App() {
   return (
     <>
-      <nav
-        style={{
-          padding: "0 20px",
-          position: "sticky",
-          top: "0",
-          backgroundColor: "pink",
-          height: "50px",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <h1>Todo App</h1>
+      <nav className="nav">
+        <h1 className="nav--logo">Todo App</h1>
       </nav>
 
-      <main
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+      <main className="main">
         {todos.map((todo, index) => {
-          //   const title = todo.title;
-          //   const description = todo.description;
           const { title, description } = todo;
           return <Todo key={index} title={title} description={description} />;
         })}
