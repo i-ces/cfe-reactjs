5a3e0f0aad96b10da3e4a6014913b86b4857440a
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2024-12-24 21:53:35 +0545

    seperated css and add useState

--- AnsarulHaq/todo-list/src/App.js
+++ AnsarulHaq/todo-list/src/App.js
@@ -1,23 +1,39 @@
 import Todo from "./components/Todo";
+import { todos } from "./data/todo";
+import { useState } from "react";
 
 function App() {
+    const [task, setTask] = useState("");
+    const addTask=()=>{
+       
+    }
+    const deleteTask=()=>{
+        
+    }
+    const deleteAllTask=()=>{
+        
+    }
   return (
-      <>
-          <nav style={
-              {
-                  display: "flex",
-                  justifyContent: "center",
-                  backgroundColor: "green",
-                  color: "white",
-                  padding: "1rem"
-              }
-          }>
-              <h1>Todo List</h1>
-          </nav>
-          <main>
-              <Todo title="Task 1" description="This is my fist task"/>
-          </main>
-      </>
+    <>
+      <nav className="nav">
+        <h1>Todo App</h1>
+      </nav>
+
+      <main className="main">
+        <div className="form">
+            <input name="task" type="text" placeholder="Enter task here!" 
+            value={task}
+            onChange={(e)=> setTask(e.target.value)}
+            />
+        </div>
+        {todos.map((todo, index) => {
+          //   const title = todo.title;
+          //   const description = todo.description;
+          const { title, description } = todo;
+          return <Todo key={index} title={title} description={description} />;
+        })}
+      </main>
+    </>
   );
 }
 
