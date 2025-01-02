39789cea73b4e474ee71d412b6d54a96553e3a1f
Seamoon Pandey <hackedasocial@gmail.com>
2025-01-03 00:19:28 +0545

    add main layout of the website in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styles from "./App.module.css";
 
-import "./App.css";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import Home from "./pages/Home";
@@ -9,11 +9,13 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 function App() {
+  const isDarkMode = false;
+
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-200">
+      <div className={`${styles.app} ${isDarkMode ? styles["app--dark"] : ""}`}>
         <Navbar />
-        <main className="pt-16">
+        <main className={styles.app__main}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
