b59439ce93a3d198a7c5f5b40c5142e3cee2d629
Seamoon Pandey <hackedasocial@gmail.com>
2025-01-03 00:22:20 +0545

    add css to home and add herosection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styles from './Home.module.css'
-
+import HeroSection from '../components/HeroSection'
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className={styles.home}>
+            <HeroSection />
+        </div>
     )
 }
